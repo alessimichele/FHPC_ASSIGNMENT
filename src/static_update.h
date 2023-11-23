0a8a1c0a5d235cdfeb8864d709066ca3d73435f9
Michele Alessi <malessi@login01.hpc.rd.areasciencepark.it>
2023-11-23 15:33:20 +0000

    update header

--- src/static_update.h
+++ src/static_update.h
@@ -1,3 +1,1 @@
-void static_update(unsigned char *current, unsigned char* next, int k,  int n,  int s, int rank, int size, int rows_per_process);
 void static_update_OpenMP(unsigned char *current, unsigned char* next, int k,  int n,  int s);
-void static_update_MPI(unsigned char *current, unsigned char* next, int k,  int n,  int s, int rank, int size, int rows_per_process);
