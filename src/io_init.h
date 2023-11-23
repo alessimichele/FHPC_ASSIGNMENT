228410740aca0e9d20ef820c79461daa3a17cd18
Michele Alessi <malessi@login01.hpc.rd.areasciencepark.it>
2023-11-23 15:32:50 +0000

    update of io_init file

--- src/io_init.h
+++ src/io_init.h
@@ -1,3 +1,3 @@
-void write_pgm_image( void *grid, int maxval, int xsize, int ysize, const char *file_name);
-void read_pgm_image( void **grid, int *maxval, int *xsize, int *ysize, const char *file_name);
-void init_serial(const char *file_name, int k);
+void write_pgm_image( void *grid, int maxval, int xsize, int ysize,  char *file_name);
+void read_pgm_image( void **grid, int *maxval, int *xsize, int *ysize,  char *file_name);
+void init_serial( char *file_name, int k);
