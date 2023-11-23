ffbfb59ca39b7e79ff0cc4daa4c55874f9543b2e
Michele Alessi <malessi@login01.hpc.rd.areasciencepark.it>
2023-11-23 19:30:43 +0000

    updating headers...

--- src/wave_update.h
+++ src/wave_update.h
@@ -1,1 +1,2 @@
-void ordered_update_finite(unsigned char* grid, unsigned char* next, int k, int n, int s );
+void wave_update(unsigned char* grid, unsigned char* next, int k, int n, int s );
+unsigned int* recoverSquare(int k, int index, int radius);
