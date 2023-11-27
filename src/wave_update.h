343f63f712e7ac73cf2d670416e1a954f1d45c43
Michele Alessi <malessi@login01.hpc.rd.areasciencepark.it>
2023-11-27 10:50:20 +0000

    added wave updated working

--- src/wave_update.h
+++ src/wave_update.h
@@ -1,3 +1,5 @@
-void wave_update(unsigned char* grid, unsigned char* next, int k, int n, int s );
-unsigned int* recoverSquare(int k, int index, int radius);
+void wave_update(unsigned char *grid, unsigned char* next, int k,  int n,  int s, int size, int rank);
 int map_even_grid(int rand_cell_idx, int k);
+unsigned int* recoverSquare(int k, int index, int radius);
+void wave_update_OpenMP(unsigned char* grid, unsigned char* next, int k, int n, int s );
+void wave_update_MPI(unsigned char *grid, unsigned char* next, int k, int n, int s);
