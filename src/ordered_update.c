ec8574e7490b95bc00ca38f43c33e94407eb1a1e
Marco Carollo <mcarol00@login01.hpc.rd.areasciencepark.it>
2023-11-28 22:56:22 +0000

    functioning mpi implementation

--- src/ordered_update.c
+++ src/ordered_update.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <mpi.h>
+#include <omp.h>
 #include "ordered_update.h"
 #include "io_init.h"
 
@@ -63,7 +65,6 @@ void ordered_update_OpenMP(unsigned char* grid, int k, int n, int s, int rank, i
     }
     return;
 }
-
 void ordered_update_MPI(unsigned char* grid, int k, int n, int s, int rank, int size, int my_rows_number){
     /*
     evolve the current state of the game of life grid for n using the ordered 
@@ -79,84 +80,95 @@ void ordered_update_MPI(unsigned char* grid, int k, int n, int s, int rank, int 
     */
  
     unsigned char* previous_row = (unsigned char*)malloc(k*sizeof(unsigned char));
-  
+    unsigned char* next_row = (unsigned char*)malloc(k*sizeof(unsigned char));
+    int next_tag = 1000000;
     for (int step=0; step<n;step++){
         MPI_Status status;
-        // last process send to first process its last row
-        if(rank==size-1) MPI_Ssend(grid + k*(my_rows_number-1), k, MPI_UNSIGNED_CHAR, (rank+1)%size, step*size+rank, MPI_COMM_WORLD);
-        // first process receive from last process its last row
-        if(rank==0) MPI_Recv(previous_row, k, MPI_UNSIGNED_CHAR, (rank-1+size)%size, step*size+rank, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
-        MPI_Barrier(MPI_COMM_WORLD);
-
-        // first process to the ordered update, and all other process wait until first process ends the ordered update of its rows
-        // then first process send its last row to second process, and second process receive it
-        // then second process to the ordered update, and all other process wait until second process ends the ordered update of its rows
-        // then second process send its last row to third process, and third process receive it
-        // and so on...
-        // at the end the "size-2" process send its last row to the "size-1" process, and the "size-1" process receive it
-        // then the "size-1" process to the ordered update, and all other process wait until the "size-1" process ends the ordered update of its rows
-        // at this point we can go to the next step
-
-        for (int i = 0; i < size; i++) {
-            if (rank == i) {
-                printf("rank %d is updating...\n", rank);
-                // updated first row
-                #pragma omp parallel for ordered  
-                for (int j=0; j<k; j++){
-                    int sum;
-                    int prev_col = (j - 1 + k)%k;
-                    int next_col = (j + 1 + k)%k;
-                    int next_row = 1;
-
-                    sum = previous_row[prev_col] + 
-                    previous_row[j] +
-                    previous_row[next_col] + 
-                    grid[next_row*k+j] + 
-                    grid[next_row*k+prev_col] + 
-                    grid[next_row*k+next_col] +
-                    grid[0*k+prev_col] +
-                    grid[0*k+next_col];
-
-                    grid[j] = (sum > 765 || sum < 510) ? 0 : 255; 
-                }
-                // updated the other 'my_rows_number-1' rows
-                #pragma omp parallel for ordered
-                for (int i = 1; i < my_rows_number; i++){ 
-                    #pragma omp ordered
-                    for (int j = 0; j < k; j++){ 
-                        int next_row = (i+1+k)%k;
-                        int previous_row = (i-1+k)%k;
-                        int next_column = (j+1+k)%k;
-                        int previous_column = (j-1+k)%k;
-                        int sum = grid[previous_row + previous_column] + 
-                            grid[previous_row + next_column] +
-                            grid[next_row + previous_column] + 
-                            grid[next_row + next_column] + 
-                            grid[previous_row + j] +
-                            grid[next_row + j] + 
-                            grid[i*k + previous_column] + 
-                            grid[i*k + next_column];
-
-                        grid[i*k+j] = (sum > 765 || sum < 510) ? 0 : 255;
-                    }
-                }
-
-                if (rank != size - 1) {
-                    // Send last row to the next process
-                    MPI_Ssend(grid + k*(my_rows_number-1), k, MPI_UNSIGNED_CHAR, rank + 1, step*size+rank, MPI_COMM_WORLD);
-                }
-            } else if (rank == i + 1) {
-                // Receive last row from the previous process
-                MPI_Recv(previous_row, k, MPI_UNSIGNED_CHAR, rank - 1, step*size+rank, MPI_COMM_WORLD, &status);
-            }
-
-            // Ensure all processes have finished this iteration before moving to the next one
-            MPI_Barrier(MPI_COMM_WORLD);
+        MPI_Request request;
+        //send operations have tag = step*size + rank
+        //receive operations have tag = step*size + rank +- 1
+        if (rank==size-1){
+            MPI_Send(grid + k*(my_rows_number-1), k, MPI_UNSIGNED_CHAR, 0, 0, MPI_COMM_WORLD);
+               
         }
+        if (rank!=0){
+            //send my first row to rank-1
+            MPI_Isend(grid, k, MPI_UNSIGNED_CHAR, rank-1, next_tag+ step*size +(rank), MPI_COMM_WORLD, &request);
+            //receive my last row from rank+1
+            MPI_Recv(next_row, k, MPI_UNSIGNED_CHAR, (rank + 1)%size,next_tag+ step*size+((rank+1)%size), MPI_COMM_WORLD, &status);
+            //receive previous row from rank-1
+            MPI_Recv(previous_row, k, MPI_UNSIGNED_CHAR, rank - 1, step*size+(rank-1), MPI_COMM_WORLD, &status);
+        
+        }else{
+            MPI_Recv(previous_row, k, MPI_UNSIGNED_CHAR, size-1, 0, MPI_COMM_WORLD, &status);
+            MPI_Recv(next_row, k, MPI_UNSIGNED_CHAR, 1,next_tag+ step*size+(rank+1), MPI_COMM_WORLD, &status);
+        }
+
+        //update the first row
+        for (int j=0; j<k; j++){
+            int sum;
+            int prev_col = (j - 1 + k)%k;
+            int next_col = (j + 1 + k)%k;
+            int next_row = 1;
+            sum = previous_row[prev_col] + 
+            previous_row[j] +
+            previous_row[next_col] + 
+            grid[next_row*k+j] + 
+            grid[next_row*k+prev_col] + 
+            grid[next_row*k+next_col] +
+            grid[0*k+prev_col] +
+            grid[0*k+next_col];
+            grid[j] = (sum > 765 || sum < 510) ? 0 : 255; 
+        }
+    
+
+        for (int i=1; i<my_rows_number-1; i++){
+            for (int j=0; j<k; j++){
+                int sum;
+                int prev_col = (j - 1 + k)%k;
+                int next_col = (j + 1 + k)%k;
+                int prev_row = i-1;
+                int next_row = i+1;
+                sum = grid[prev_row*k+prev_col] + 
+                grid[prev_row*k+j] +
+                grid[prev_row*k+next_col] +
+                grid[next_row*k+prev_col] + 
+                grid[next_row*k+next_col] +
+                grid[next_row*k+j] +
+                grid[i*k+prev_col] +
+                grid[i*k+next_col];
+                grid[i*k+j] = (sum > 765 || sum < 510) ? 0 : 255; 
+            }
+        }
+        //update last row
+        for (int j=0; j<k; j++){
+            int sum;
+            int prev_col = (j - 1 + k)%k;
+            int next_col = (j + 1 + k)%k;
+            int prev_row = my_rows_number-2;
+            sum = grid[prev_row*k+prev_col] + 
+            grid[prev_row*k+j] +
+            grid[prev_row*k+next_col] +
+            grid[(my_rows_number-1)*k+prev_col] + 
+            grid[(my_rows_number-1)*k+next_col] +
+            next_row[prev_col] +
+            next_row[next_col] +
+            next_row[j];
+            grid[(my_rows_number-1)*k+j] = (sum > 765 || sum < 510) ? 0 : 255; 
+        }
+    
+        if (rank!=size-1){
+            //send my last row to rank+1
+            MPI_Isend(grid + k*(my_rows_number-1), k, MPI_UNSIGNED_CHAR, rank+1, step*size +(rank), MPI_COMM_WORLD, &request);
+        }
+
+        if (rank == 0){
+            MPI_Send(grid, k, MPI_UNSIGNED_CHAR, size-1,next_tag+ step*size +(rank), MPI_COMM_WORLD);
+        }
+        
         if ((step+1)%s==0){
             char *file_path = (char*)malloc(32*sizeof(char) + 1);
-            strcpy(file_path, "files/static/");
+            strcpy(file_path, "files/ordered/");
 
             char *fname = (char*)malloc(20*sizeof(char) + 1);
             snprintf(fname, 20, "snapshot_%05d.pgm", step+1);
@@ -167,7 +179,9 @@ void ordered_update_MPI(unsigned char* grid, int k, int n, int s, int rank, int 
             free(fname);
             free(file_path);  
         }
-        free(previous_row);
+       
     }
+    free(previous_row);
+    free(next_row);
     return;
 }
